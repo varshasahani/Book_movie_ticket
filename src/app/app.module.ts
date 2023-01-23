8d069d2c06439778c39e4080f51ebfc5e874c8ea
varshasahani <sahani.varsha04@gmail.com>
2023-01-23 16:25:37 +0530

    handling material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
+    MaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
