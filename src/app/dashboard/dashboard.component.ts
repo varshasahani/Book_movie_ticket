9167371e86fc39858ca72504565940e5b41efbbb
varshasahani <sahani.varsha04@gmail.com>
2023-01-28 16:50:31 +0530

    on Select ,display json data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component,OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
 import { MovieService } from '../movie.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,39 +15,60 @@ export class DashboardComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
   cols=4;
   public movies:any[]=[];
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        this.cols=1
-        return [
-          { title: 'Card 1', cols: 1, rows: 2 },
-          { title: 'Card 2', cols: 1, rows: 2 },
-          { title: 'Card 3', cols: 1, rows: 2 },
-          { title: 'Card 4', cols: 1, rows: 2 }
-        ];
-      }
+  apiResponse:any=[];
+  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     if (matches) {
+  //       this.cols=1
+  //       return [
+  //         { title: 'Card 1', cols: 1, rows: 2 },
+  //         { title: 'Card 2', cols: 1, rows: 2 },
+  //         { title: 'Card 3', cols: 1, rows: 2 },
+  //         { title: 'Card 4', cols: 1, rows: 2 }
+  //       ];
+  //     }
 
-      return [
-        { title: 'Card 1', cols: 1, rows: 2 },
-        { title: 'Card 2', cols: 1, rows: 2 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 2 }
-      ];
-    })
-  );
+  //     return [
+  //       { title: 'Card 1', cols: 1, rows: 2 },
+  //       { title: 'Card 2', cols: 1, rows: 2 },
+  //       { title: 'Card 3', cols: 1, rows: 2 },
+  //       { title: 'Card 4', cols: 1, rows: 2 }
+  //     ];
+  //   })
+  // );
 
   constructor(private breakpointObserver: BreakpointObserver,
     private dialog:MatDialog,
     private _movieService:MovieService) {}
 
     ngOnInit(){
+      
       this._movieService.getMovies()
-      .subscribe(data=>this.movies=data);
+      .subscribe(data=>{
+        this.movies=data;
+        this.apiResponse=data;
+      });
     }
 
     openLogin(){
       this.dialog.open(LoginComponent)
     
+    }
+
+    // onSelect(langauge){
+    //   this._movieService.getMovies().pipe(
+    //     filter((item)=>item.langauge == langauge))
+    //     .subscribe(data=>this.movies=data);
+      
+    // }
+
+    onChange($event:any){
+
+      let filterData=_.filter(this.apiResponse,(item)=>{
+        return item.langauge.toLowerCase()==$event.value.toLowerCase();
+      })
+      this.movies=filterData;
+      
     }
     
 }
